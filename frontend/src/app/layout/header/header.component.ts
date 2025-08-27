93dcc548566dc0c23b81133a47abbb43fa9b0a67
Erik-Mfa <erikmfa190230@gmail.com>
2025-08-27 19:18:09 -0400

    header buttons

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -6,4 +6,25 @@ import { Component } from "@angular/core";
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  
+  scrollToFaq(): void {
+    const faqElement = document.querySelector('app-faq');
+    if (faqElement) {
+      faqElement.scrollIntoView({ 
+        behavior: 'smooth', 
+        block: 'start' 
+      });
+    }
+  }
+  
+  scrollToInquiry(): void {
+    const inquiryElement = document.querySelector('app-inquiry-form');
+    if (inquiryElement) {
+      inquiryElement.scrollIntoView({ 
+        behavior: 'smooth', 
+        block: 'start' 
+      });
+    }
+  }
+}
